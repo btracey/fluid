0429706425564dc7795bcfb84062e081718aac35
btracey <bdtinternet@gmail.com>
2014-09-15 18:22:26 -0700

    changes

--- fluid2d/predict.go
+++ fluid2d/predict.go
@@ -14,5 +14,5 @@ func TurbKinVisc(tau ReynoldsStress, S StrainRate, eps float64) fluid.TurbKinVis
 		2*tau.UV()*S.Symmetric[1] +
 		tau.VV()*S.Symmetric[2]
 
-	return (-1.0 / 2.0) * (sum / (norm + eps))
+	return fluid.TurbKinVisc((-1.0 / 2.0) * (sum / (norm*norm + eps)))
 }
